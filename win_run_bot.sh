68808aa6ca18f0f8b1841c0f7a9a30170b9b49c6
kdao167 <45550338+kdao167@users.noreply.github.com>
2025-03-12 19:31:31 -0700

    test

--- win_run_bot.sh
+++ win_run_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t 153-proj . && winpty docker run -e OPENAI_API_KEY=$OPENAI_API_KEY -e DISCORD_TOKEN=$DISCORD_TOKEN MISTRAL_API_KEY=$MISTRAL_API_KEY --rm -ti -v $(pwd)/logs:/app/logs 153-proj
+docker build -t 153-proj . && winpty docker run -e OPENAI_API_KEY=$OPENAI_API_KEY -e DISCORD_TOKEN=$DISCORD_TOKEN -e MISTRAL_API_KEY=$MISTRAL_API_KEY --rm -ti -v $(pwd)/logs:/app/logs 153-proj
